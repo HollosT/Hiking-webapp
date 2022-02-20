9e40cd0eba76a377b4b7933ba34571bf1945b363
HollosT <hollos.tamas@ravensoft.hu>
2022-02-20 15:29:19 +0100

    Initial JS

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+document.getElementById('card1').addEventListener('click', function (){
+document.getElementById('article').classList.toggle('hide')
+});
+
+
+let content = 'Hello'
+document.getElementById('FaF').addEventListener('click', function () {
+if (document.getElementById('content').innerHTML = '') {
+    document.getElementById('content').innerHTML = content
+} else {
+    document.getElementById('content').innerHTML = '';
+}
+})
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Hello") {
+      x.innerHTML = "Swapped text!";
+    } else {
+      x.innerHTML = "Hello";
+    }
+  }
+
+ function addContent () {
+     
+ } 
