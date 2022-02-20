97fcedd71bf63d3c03883a7772232ec4875bc20f
HollosT <hollos.tamas@ravensoft.hu>
2022-02-20 19:17:21 +0100

    adding the rest of JS
    Experimenting with the filter but its not working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,63 @@ document.getElementById('card2').addEventListener('click', function (){
   document.getElementById('article2').classList.toggle('hide')
   });
 
+document.getElementById('card3').addEventListener('click', function (){
+  document.getElementById('article3').classList.toggle('hide')
+  });
 
+  document.getElementById('card4').addEventListener('click', function (){
+    document.getElementById('article4').classList.toggle('hide')
+    });
+  
+    document.getElementById('card5').addEventListener('click', function (){
+      document.getElementById('article5').classList.toggle('hide')
+      });
+
+    
+      document.getElementById('card6').addEventListener('click', function (){
+        document.getElementById('article6').classList.toggle('hide')
+        });
+      
+//* Filter
+
+filter('all');
+
+function filter(c) {
+  let x, i,
+  x = document.getElementsByClassName('cardFilterItem');
+  if (c == 'all') c = ''; 
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], 'show');
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
+  }
+}
+
+function addClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.className.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function removeClass(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.className.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+
+//* Function 
 function togglePopupOne(){
   document.getElementById('popup-1').classList.toggle("active")
 }
@@ -29,4 +85,53 @@ function togglePopupFive(){
 
 function togglePopupSix(){
   document.getElementById('popup-6').classList.toggle("active")
+}
+
+function togglePopupSeven(){
+  document.getElementById('popup-7').classList.toggle("active")
+}
+
+function togglePopupEight(){
+  document.getElementById('popup-8').classList.toggle("active")
+}
+
+function togglePopupNine(){
+  document.getElementById('popup-9').classList.toggle("active")
+}
+
+function togglePopupTen(){
+  document.getElementById('popup-10').classList.toggle("active")
+}
+
+function togglePopupEleven(){
+  document.getElementById('popup-11').classList.toggle("active")
+}
+
+function togglePopupTwelve(){
+  document.getElementById('popup-12').classList.toggle("active")
+}
+
+function togglePopupThirteen(){
+  document.getElementById('popup-13').classList.toggle("active")
+}
+
+function togglePopupFourteen(){
+  document.getElementById('popup-14').classList.toggle("active")
+}
+
+function togglePopupFifteen(){
+  document.getElementById('popup-15').classList.toggle("active")
+}
+
+
+function togglePopupSixteen(){
+  document.getElementById('popup-16').classList.toggle("active")
+}
+
+function togglePopupSeventeen(){
+  document.getElementById('popup-17').classList.toggle("active")
+}
+
+function togglePopupEighteen(){
+  document.getElementById('popup-18').classList.toggle("active")
 }
