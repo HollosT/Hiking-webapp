9d7540e1806bc124529a43d0aee8ee16062fc0a6
HollosT <hollos.tamas@ravensoft.hu>
2022-02-20 21:17:37 +0100

    Deletin the filter section

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,42 +23,6 @@ document.getElementById('card3').addEventListener('click', function (){
         document.getElementById('article6').classList.toggle('hide')
         });
       
-//* Filter
-
-filter('all');
-
-function filter(c) {
-  let x, i,
-  x = document.getElementsByClassName('cardFilterItem');
-  if (c == 'all') c = ''; 
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], 'show');
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], 'show');
-  }
-}
-
-function addClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.className.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
-}
-
-function removeClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.className.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
 
 
 
